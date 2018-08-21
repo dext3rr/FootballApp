d8f889de2ca174edd5633e8dd815196bc74c801c
Martin <haladyn.martin@gmail.com>
2018-08-21 17:15:09 +0200

    aaa

--- FootballApp-SPA/src/app/app.module.ts
+++ FootballApp-SPA/src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-<<<<<<< HEAD
-
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
-=======
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -14,20 +9,10 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
->>>>>>> ef3a1da
 
 @NgModule({
    declarations: [
       AppComponent,
-<<<<<<< HEAD
-      ValueComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule
-   ],
-   providers: [],
-=======
       ValueComponent,
       NavComponent,
       HomeComponent,
@@ -41,7 +26,6 @@ import { RegisterComponent } from './register/register.component';
    providers: [
       AuthService
    ],
->>>>>>> ef3a1da
    bootstrap: [
       AppComponent
    ]
